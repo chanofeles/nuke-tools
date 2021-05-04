298343b682752cc160f1e2c7f5d5ae807042cf1d
Keyframe <chanofeles@hotmail.com>
2021-05-03 22:54:51 -0400

    Create init.py
    Nuke init file
    Last updated May 3rd 2021

---
+++ init.py
@@ -0,0 +1,13 @@
+##Nuke settings
+###Last updated May 3rd 2021
+
+##NukeSurvivalToolkit
+nuke.pluginAddPath("./NukeSurvivalToolkit")
+
+##W_hotbox
+nuke.pluginAddPath("./W_hotbox")
+
+
+
+##KnobScripter
+#from KnobScripter import *
